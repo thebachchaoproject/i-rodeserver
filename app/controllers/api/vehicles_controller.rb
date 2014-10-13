e080c847820222a6bca1b093808f97e093aca184
Ashwin Murali <mail@cruisemaniac.com>
2014-10-14 05:12:50 +0530

    done done

--- app/controllers/api/vehicles_controller.rb
+++ app/controllers/api/vehicles_controller.rb
@@ -7,7 +7,11 @@ module Api
     end
 
     def show
-      respond_with(:status => :ok, data: "welcome to safetyapp")      
+      puts "Params: #{params}"
+      render json: Vehicle.where("vehicle_number like '%#{params[:vehicle_no]}%'").to_json(:include => {:vehiclereviews => 
+                                {:except => [:vehicle_id, :id, :created_at, :updated_at]}}, 
+                                :except => [:created_at, :updated_at, :id])
+      #respond_with(:status => :ok, data: "welcome to safetyapp")      
     end
 
     def update
@@ -15,16 +19,38 @@ module Api
     end
 
     def create
-      puts "Safetyapp - Accepted Params: #{accepted_params}"
       puts "Safetyapp - Request: #{request.params}"
-      
+      puts "that was before we formatted"
+      puts " =--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--="
+      mypayload = %w{safety_rating date_of_travel driver_name gps_or_location review photo_link }
+      params[:vehicle][:vehiclereviews_attributes] = []
+      myhash = {}
+      params.each do |key, value|
+        puts "inside params.each #{key}"
+        if mypayload.include? key
+          puts "inside mypayload #{value}"
+          myhash[key] = value
+          params.delete(key)
+
+        end
+        
+      end
+      params[:vehicle][:vehiclereviews_attributes] << myhash
+      puts "and we're done formatting"
+      puts " =--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--="
+      puts params
+      #render :json => {:status => 'bleh'}, :status => 500
+
       if Vehicle.where("vehicle_number like '%#{accepted_params[:vehicle_number]}%'").empty?
-        render json: Vehicle.create(accepted_params)
+        puts "vehicle not found"
+        render json: Vehicle.create(accepted_params), :status => 201
       else
+        puts "vehicle found... moving on"
         @v = Vehicle.where("vehicle_number like '%#{accepted_params[:vehicle_number]}%'").first
+        puts accepted_params[:vehiclereviews_attributes]
         @v.vehiclereviews.build(accepted_params[:vehiclereviews_attributes])
         if @v.save 
-          render :json => {:status => "Your data has been saved successfully!"}, :status => 200
+          render :json => {:status => "Your data has been saved successfully!"}, :status => 201
         else 
           render :json => {:status => "Failed to save. Please try again!"}, :status => 500
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :vehicles, only: [:index, :show, :update, :create, :destroy]
+    get '/vehicles/:vehicle_no', to: 'vehicles#show'
+    resources :vehicles, only: [:index, :update, :create, :destroy]
+
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
